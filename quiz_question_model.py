4ba11a8b6e7e13f8e06ab34fe08dd5d6902fa74d
wajeehrehman35 <81848256+wajeehrehman35@users.noreply.github.com>
2021-09-04 22:59:41 +0500

    Update quiz_question_model.py

--- quiz_question_model.py
+++ quiz_question_model.py
@@ -7,38 +7,3 @@ class Quiz_Store:
         
         self.question=question
         self.answer=answer
-       # self.score=0
-      
-    
-    #def get_question(self):
-        
-        #return(self.question)
-    
-   # def get_answer(self):
-        
-       # return(self.answer)
-    
-   # def check_answer(self,ans):
-        
-        #if(ans==self.answer):
-           # print("Correct")
-           # self.score+=1
-            #print(self.score)
-    
-   # def next_question(self,obj):
-       # obj.question=self.question
-    
-        
-
-
-
-#test=Quiz_Store(question_data)
-#print(test.text)
-#print(test.get_question())
-#print(test.get_answer())
-
-#test.check_answer('True')
-#print(test.question)
-#print(test.answer)
-        
-    
